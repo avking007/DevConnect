5c88dcbd7a64a597ead4ca576f565ebf71423f44
Anish Varshney <avanvarshney@gmail.com>
2020-06-22 19:04:28 +0530

    added post create view

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
-// @route GET api/PROFILE
-// @desc  TEST ROUTE
-// @access PUBLIC
-router.get('/', (req, res) => {
-  res.send('post route');
-});
+// @route POST api/PROFILE
+// @desc  add post ROUTE
+// @access private
+
+router.post(
+  '/',
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) return res.status(400).json({ error: err.array() });
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newpost = new Post({
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+        avatar: user.avatar,
+      });
+      const new_post = await newpost.save();
+      res.json(new_post);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
