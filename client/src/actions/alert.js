8a081c573aeddcca10a408bd3651e963d7c25e25
Anish Varshney <avanvarshney@gmail.com>
2020-07-04 13:48:09 +0530

    added alert action for redux

---
+++ client/src/actions/alert.js
@@ -0,0 +1,7 @@
+import { SET_ALERT, REMOVE_ALERT } from './types';
+import uuid from 'uuid';
+
+export const set_alert = (msg, alertType) => (dispatch) => {
+  const id = uuid.v4();
+  dispatch({ type: SET_ALERT, payload: { msg, alertType, id } });
+};
