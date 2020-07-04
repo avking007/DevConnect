a0382c197d093225d8401a2ecffab31c994f6d5d
Anish Varshney <avanvarshney@gmail.com>
2020-07-04 17:53:35 +0530

    created alert action

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,9 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
-import uuid from 'uuid';
+import { v4 } from 'uuid';
 
 export const set_alert = (msg, alertType) => (dispatch) => {
-  const id = uuid.v4();
+  const id = v4();
   dispatch({ type: SET_ALERT, payload: { msg, alertType, id } });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000);
 };
