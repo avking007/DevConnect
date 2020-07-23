461a8f9e7c8b574d2f94583a84a72aa30734fc95
Anish Varshney <avanvarshney@gmail.com>
2020-07-23 21:22:03 +0530

    added Post model

--- models/Post.js
+++ models/Post.js
@@ -46,6 +46,10 @@ const postSchema = new mongoose.Schema({
       },
     },
   ],
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 module.exports = Post = mongoose.model('post', postSchema);
