c4913903573f0be2d6cd86d3338bced75b7c7875
Anish Varshney <=>
2023-01-30 16:29:58 +0530

    prepare for deployement

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use('/api/users', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/post'));
+
+// server static assets
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
