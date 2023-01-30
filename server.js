bb500534f24af4e3a01fc2261533c384ae4d14d5
Anish Varshney <avanvarshney@gmail.com>
2023-01-30 17:31:59 +0530

    removed console log

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-console.log(path.join(__dirname, '/client/build'), path.resolve(__dirname, 'client', 'build', 'index.html') )
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server started on ${PORT}`));
